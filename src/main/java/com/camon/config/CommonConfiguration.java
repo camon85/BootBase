2bd5d0735ba84462624c679874f981bdd98a5900
camon <camon85@gmail.com>
2015-12-20 23:29:18 +0900

    api 인터셉터

--- src/main/java/com/camon/config/CommonConfiguration.java
+++ src/main/java/com/camon/config/CommonConfiguration.java
@@ -1,18 +1,21 @@
 package com.camon.config;
 
+import com.camon.interceptor.EventInterceptor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * Created by jooyong on 2015-12-20.
  */
 @Configuration
 @Slf4j
-public class CommonConfiguration {
+public class CommonConfiguration extends WebMvcConfigurerAdapter {
     static {
         log.info("### CommonConfiguration");
     }
@@ -24,5 +27,10 @@ public class CommonConfiguration {
         filter.setEncoding("UTF-8");
         filter.setForceEncoding(true);
         return filter;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new EventInterceptor()).addPathPatterns("/api/**");
     }
 }
